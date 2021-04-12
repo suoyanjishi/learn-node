0c5ef9feafca49f22d79a8b09f134685e475430b
suoyanjishi <827781273@qq.com>
2021-04-12 23:26:39 +0800

    rock game

---
+++ index.js
@@ -0,0 +1,25 @@
+let playerAction = process.argv[process.argv.length - 1];
+console.log(playerAction);
+
+let random = Math.random() * 3;
+let computerAction;
+
+if (random < 1) {
+  computerAction = "rock";
+} else if (random > 2) {
+  computerAction = "scissor";
+} else {
+  computerAction = "pager";
+}
+
+if (computerAction === playerAction) {
+  console.log("平局");
+} else if (
+  (computerAction === "rock" && playerAction === "paper") ||
+  (computerAction === "scissor" && playerAction === "rock") ||
+  (computerAction === "paper" && playerAction === "scissor")
+) {
+  console.log("你赢了");
+} else {
+  console.log("你输了");
+}
