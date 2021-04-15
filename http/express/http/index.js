19dcf3d8e232dced40f59b71b6efabe705b4b7bd
suoyanjishi <827781273@qq.com>
2021-04-16 00:16:16 +0800

    fix: express koa

--- http/express/http/index.js
+++ http/express/http/index.js
@@ -1,63 +1,75 @@
-const querystring = require("querystring");
-const http = require("http");
-const url = require("url");
 const fs = require("fs");
-
 const game = require("./game");
+const express = require("express");
 
 let playerWon = 0;
 let playerLastAction = null;
 let sameCount = 0;
 
-http
-  .createServer(function (request, response) {
-    const parseUrl = url.parse(request.url);
+const app = express();
 
-    if (parseUrl.pathname === "/favicon.ico") {
-      response.writeHead(200);
-      response.end();
+app.get("/favicon.ico", function (request, response) {
+  response.status(200);
+  return;
+});
+
+app.get(
+  "/game",
+  function (request, response, next) {
+    if (playerWinCount >= 3 || sameCount === 9) {
+      response.status(500);
+      response.send("我不会再玩了！");
       return;
     }
-    if (parsedUrl.pathname === "/game") {
-      const query = querystring.parse(parseUrl.query);
-      const playerAction = query.action;
+    next();
+    if (response.playerWon) {
+      playerWinCount++;
+    }
+  },
+  function (request, response, next) {
+    const query = request.query;
+    const playerAction = query.action;
 
-      if (playerWon >= 3 || sameCount === 9) {
-        response.writeHead(500);
-        response.end("我再也不和你玩了！");
-        return;
-      }
+    if (!playerAction) {
+      response.status(400);
+      response.send();
+      return;
+    }
 
-      if (playerLastAction && playerAction === playerLastAction) {
-        sameCount++;
-      } else {
-        sameCount = 0;
-      }
-      playerLastAction = playerAction;
-
+    if (lastPlayerAction === playerAction) {
+      sameCount++;
       if (sameCount >= 3) {
-        response.writeHead(400);
-        response.end("你作弊!");
+        response.status(400);
+        response.send("你作弊！我再也不玩了");
         sameCount = 9;
         return;
       }
-
-      const gameResult = game(playerAction);
-
-      response.writeHead(200);
-
-      if (gameResult === 0) {
-        response.end("平局!");
-      } else if (gameResult === 1) {
-        response.end("你赢了!");
-        playerWon++;
-      } else {
-        response.end("你输了!");
-      }
+    } else {
+      sameCount = 0;
     }
+    lastPlayerAction = playerAction;
 
-    if (parsedUrl.pathname === "/") {
-      fs.createReadStream(__dirname + "/index.html").pipe(response);
+    response.playerAction = playerAction;
+    next();
+  },
+  function (request, response) {
+    const playerAction = response.playerAction;
+    const result = game(playerAction);
+
+    response.status(200);
+    if (result === 0) {
+      response.send("平局");
+    } else if (result === -1) {
+      response.send("你输了");
+    } else {
+      response.send("你赢了");
+      response.playerWon = true;
     }
-  })
-  .listen(3000);
+  }
+);
+
+app.get("/", function (request, response) {
+  response.send(fs.readFileSync(__dirname + "./index.html", "utf-8"));
+});
+
+app.listen(3000);
